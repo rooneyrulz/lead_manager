7c10097e98a353caaff69373b9b994bea4d841b0
rooneyrulz <roony0782045336@gmail.com>
2019-10-21 16:00:47 +0530

    lead modal setup

--- frontend/src/components/Lead.js
+++ frontend/src/components/Lead.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
+import LeadModal from './modals/LeadModal';
+
 const Lead = () => {
   return (
-    <div>
-      <h1>Leads</h1>
+    <div className='Lead'>
+      <header
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+      >
+        <h1 className='m-0'>Leads</h1>
+        <div className='lead-modal'>
+          <LeadModal />
+        </div>
+      </header>
+      <div className='lead-content'>Leads...</div>
     </div>
-  )
-}
+  );
+};
 
 export default Lead;
